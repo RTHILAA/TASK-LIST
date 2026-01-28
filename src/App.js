4ad15816da81d16d526f1ec387ee276051236703
anaswins350-ai <anaswins35@gmail.com>
2026-01-28 21:43:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './App.css'
+import { Plus } from 'lucide-react';
 
 export default function App() {
   return (
-    <div>test</div>
+    <div className="App">
+    <div className="title">
+      <h1>Today's Tasks</h1>
+      <p>You're all done!</p>
+    </div>
+    <div className="task">
+     <form method='post' className='form'>
+     <input type="text" placeholder='Add a new task...' required/>
+     <button type="submit" className="btn-submit">
+      <Plus size={24} />
+    </button>
+     </form>
+    </div>
+</div>
   )
 }
