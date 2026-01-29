a9facf8b691dba2385d6ae5a1464d6a80dfff87e
anaswins350-ai <anaswins35@gmail.com>
2026-01-29 05:08:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import React from 'react'
 import './App.css'
-import { Plus } from 'lucide-react';
+import { Plus, Trash2, Pencil } from 'lucide-react';
+import { useState } from 'react';
 
 export default function App() {
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState({})
+
+  const handleChange = (e) => {
+    const id = e.target.id
+    const value = e.target.value
+    setTask({ ...task, [id]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newTask = { ...task, id: Date.now() }
+    setTasks([...tasks, newTask])
+    setTask({})
+  }
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  
+  const handleEdit= (id) => {
+    setTask(tasks.find((task) => task.id === id))
+  }
+  
   return (
     <div className="App">
-    <div className="title">
-      <h1>Today's Tasks</h1>
-      <p>You're all done!</p>
+      <div className="title">
+        <h1>Today's Tasks</h1>
+        <p>You're all done!</p>
+      </div>
+      <div className="task">
+        <form method='post' className='form' onSubmit={handleSubmit}>
+          <input type="text" id='tsk' value={task.tsk || ""} placeholder='Add a new task...' required onChange={handleChange} />
+          <button type="submit" className="btn-submit">
+            <Plus size={24} />
+          </button>
+        </form>
+      </div>
+
+      <div className="task-list">
+        {tasks.map((task) => {
+          return (
+            <ul key={task.id} className="task-item">
+              <li>{task.tsk}
+                <Pencil className="btn-edit" size={20} onClick={() => handleEdit(task.id)} />
+                <Trash2 className="btn-delete" size={20} onClick={() => handleDelete(task.id)} />
+              </li>
+            </ul>
+          )
+        }
+        )}
+      </div>
     </div>
-    <div className="task">
-     <form method='post' className='form'>
-     <input type="text" placeholder='Add a new task...' required/>
-     <button type="submit" className="btn-submit">
-      <Plus size={24} />
-    </button>
-     </form>
-    </div>
-</div>
   )
 }
